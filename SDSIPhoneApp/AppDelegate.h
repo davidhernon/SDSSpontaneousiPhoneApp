c039e9d1a7d4fcb1743d53d0691be1cbf3cfb0d0
David Hernon <david.hernon@mail.mcgill.ca>
2014-10-11 17:21:07 -0400

    Initial Commit

---
+++ SDSIPhoneApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SDSIPhoneApp
+//
+//  Created by David Hernon on 10/11/14.
+//  Copyright (c) 2014 Silent Disco Squad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SDSIPhoneAppTests/SDSIPhoneAppTests.m
@@ -0,0 +1,34 @@
+//
+//  SDSIPhoneAppTests.m
+//  SDSIPhoneAppTests
+//
+//  Created by David Hernon on 10/11/14.
+//  Copyright (c) 2014 Silent Disco Squad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SDSIPhoneAppTests : XCTestCase
+
+@end
+
+@implementation SDSIPhoneAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
